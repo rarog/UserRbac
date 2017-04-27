df6cd8d0433550e0aea860d2787798085bc87e7f
Andrej Sinicyn <rarogit@gmail.com>
2017-04-27 06:54:17 +0200

    Changing MySQL schema
    - Renaming file.
    - Adding ON UPDATE CASCADE restriction.

--- data/schema.mysql.sql
+++ data/schema.mysql.sql
@@ -1,8 +1,7 @@
-﻿
-CREATE TABLE `user_role_linker` (
+﻿CREATE TABLE `user_role_linker` (
   `user_id` int(10) unsigned NOT NULL,
   `role_id` varchar(45) NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`),
   KEY `user_role_linker_fk1_idx` (`user_id`),
-  CONSTRAINT `user_role_linker_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
+  CONSTRAINT `user_role_linker_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
