1b317d66647c260a2f5df396a94b7ee754850882
rarog <rarogit@gmail.com>
2018-04-11 22:10:45 +0200

    Reworking schemata
    - Changing current schemata to be more strict and compliant.
    - Adding schema for PostgreSQL.

--- data/schema.mysql.sql
+++ data/schema.mysql.sql
@@ -1,7 +1,7 @@
-﻿CREATE TABLE `user_role_linker` (
-  `user_id` int(10) unsigned NOT NULL,
-  `role_id` varchar(45) NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `user_role_linker_fk1_idx` (`user_id`),
-  CONSTRAINT `user_role_linker_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `user_role_linker` (
+    `user_id` BIGINT NOT NULL,
+    `role_id` VARCHAR(45) NOT NULL,
+    CONSTRAINT `user_role_linker_pk` PRIMARY KEY (`user_id`, `role_id`),
+    CONSTRAINT `user_role_linker_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    INDEX `user_role_linker_ik1` (`user_id`)
+);
