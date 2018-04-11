49aecb42e815622edcdd40163a72e633c1ce1a80
rarog <rarogit@gmail.com>
2018-04-11 22:38:20 +0200

    Raising role_id length

--- data/schema.pgsql.sql
+++ data/schema.pgsql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "user_role_linker" (
     "user_id" BIGINT NOT NULL,
-    "role_id" VARCHAR(45) NOT NULL,
+    "role_id" VARCHAR(255) NOT NULL,
     CONSTRAINT "user_role_linker_pk" PRIMARY KEY ("user_id", "role_id"),
     CONSTRAINT "user_role_linker_fk1" FOREIGN KEY ("user_id") REFERENCES "user" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE 
 );
