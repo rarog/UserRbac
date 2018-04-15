2b8da7ffff9d839964112a972df966f5c75187a3
rarog <rarogit@gmail.com>
2018-04-15 20:28:35 +0200

    Formatting

--- src/Factory/Identity/IdentityProviderFactory.php
+++ src/Factory/Identity/IdentityProviderFactory.php
@@ -8,16 +8,19 @@ use Zend\ServiceManager\Factory\FactoryInterface;
 
 class IdentityProviderFactory implements FactoryInterface
 {
+
     /**
      * Gets identity provider
      *
-     * @param  ContainerInterface $container
-     * @param  string             $requestedName
-     * @param  null|array         $options
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param null|array $options
      * @return IdentityProvider
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        return new IdentityProvider($container->get(IdentityRoleProvider::class));
+        return new IdentityProvider(
+            $container->get(IdentityRoleProvider::class)
+        );
     }
 }
