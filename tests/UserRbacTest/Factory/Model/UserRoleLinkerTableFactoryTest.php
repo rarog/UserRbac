2b2597109a39a9c743a7044df59c01b14ce7c6a2
rarog <rarogit@gmail.com>
2018-04-16 20:08:04 +0200

    Extending test

--- tests/UserRbacTest/Factory/Model/UserRoleLinkerTableFactoryTest.php
+++ tests/UserRbacTest/Factory/Model/UserRoleLinkerTableFactoryTest.php
@@ -5,9 +5,10 @@ use PHPUnit\Framework\TestCase;
 use UserRbac\Factory\Model\UserRoleLinkerTableFactory;
 use UserRbac\Model\UserRoleLinkerTable;
 use UserRbac\Options\ModuleOptions;
-use ZfcUser\Options\ModuleOptions as ZfcUserModuleOptions;
 use Zend\Db\Adapter\AdapterInterface;
 use Zend\ServiceManager\ServiceManager;
+use ZfcUser\Options\ModuleOptions as ZfcUserModuleOptions;
+use ReflectionClass;
 
 class UserRoleLinkerTableFactoryTest extends TestCase
 {
@@ -15,14 +16,28 @@ class UserRoleLinkerTableFactoryTest extends TestCase
     public function testFactory()
     {
         $factory = new UserRoleLinkerTableFactory();
+
+        $moduleOptions = new ModuleOptions();
+
         $serviceManager = new ServiceManager();
         $serviceManager->setService('zfcuser_module_options', new ZfcUserModuleOptions());
-        $serviceManager->setService(ModuleOptions::class, new ModuleOptions());
+        $serviceManager->setService(ModuleOptions::class, $moduleOptions);
         $adapter = $this->getMockBuilder(AdapterInterface::class)
             ->disableOriginalConstructor()
             ->getMock();
         $serviceManager->setService(AdapterInterface::class, $adapter);
 
-        $this->assertInstanceOf(UserRoleLinkerTable::class, $factory($serviceManager, null));
+        $userRoleLinker = $factory($serviceManager, null);
+        $this->assertInstanceOf(UserRoleLinkerTable::class, $userRoleLinker);
+
+        $reflection = new ReflectionClass(UserRoleLinkerTable::class);
+        $property = $reflection->getProperty('tableGateway');
+        $property->setAccessible(true);
+
+        /**
+         * @var TableGatewayInterface $tableGateway
+         */
+        $tableGateway = $property->getValue($userRoleLinker);
+        $this->assertEquals('user_role_linker', $tableGateway->getTable());
     }
 }
