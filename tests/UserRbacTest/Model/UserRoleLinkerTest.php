fb3c409548902d5d0b4c3a8453362c711e35d66c
rarog <rarogit@gmail.com>
2018-04-17 08:35:17 +0200

    Adding more tests for UserRoleLinker

--- tests/UserRbacTest/Model/UserRoleLinkerTest.php
+++ tests/UserRbacTest/Model/UserRoleLinkerTest.php
@@ -7,16 +7,53 @@ use ZfcUser\Entity\User;
 
 class UserRoleLinkerTest extends TestCase
 {
+
+    public function testConstructor()
+    {
+        $user = new User();
+        $user->setId(155);
+
+        $userRoleLinker = new UserRoleLinker(
+            $user,
+            'manager'
+        );
+
+        $this->assertEquals(155, $userRoleLinker->getUserId());
+        $this->assertEquals('manager', $userRoleLinker->getRoleId());
+    }
+
     public function testSetterAndGetters()
     {
         $userRoleLinker = new UserRoleLinker();
         $userRoleLinker->setUserId(178);
         $userRoleLinker->setRoleId('manager');
+
         $this->assertEquals(178, $userRoleLinker->getUserId());
         $this->assertEquals('manager', $userRoleLinker->getRoleId());
+
         $user = new User();
         $user->setId(155);
+
         $userRoleLinker->setUser($user);
         $this->assertEquals(155, $userRoleLinker->getUserId());
+    }
+
+    public function testArraySerializableImplementations()
+    {
+        $userRoleLinker = new UserRoleLinker();
+        $userRoleLinker->setUserId(178);
+        $userRoleLinker->setRoleId('manager');
+
+        $this->assertEquals([
+            'user_id' => 178,
+            'role_id' => 'manager',
+        ], $userRoleLinker->getArrayCopy());
+
+        $userRoleLinker->exchangeArray([
+            'user_id' => 42,
+            'role_id' => 'vogon',
+        ]);
+        $this->assertEquals(42, $userRoleLinker->getUserId());
+        $this->assertEquals('vogon', $userRoleLinker->getRoleId());
     }
 }
